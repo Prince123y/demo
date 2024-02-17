065bedd11eb4a7f00f79060bcfca701b38d15e39
princ <princeyadav764148@gmail.com>
2024-02-17 09:58:35 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/VerifiyOTPActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class VerifiyOTPActivity extends AppCompatActivity {
+
+    private EditText inputCode1, inputCode2, inputCode3, inputCode4, inputCode5, inputCode6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verifiy_otpactivity);
+
+        inputCode1 = findViewById(R.id.inputcode1);
+        inputCode2 = findViewById(R.id.inputcode2);
+        inputCode3 = findViewById(R.id.inputcode3);
+        inputCode4 = findViewById(R.id.inputcode4);
+        inputCode5 = findViewById(R.id.inputcode5);
+        inputCode6 = findViewById(R.id.inputcode6);
+
+    }
+
+        });
+}
